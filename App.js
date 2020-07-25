088b4f31981f6c0e2fd1b5dd99917dfb1b08bc24
elywin <lukeeraelywin@gmail.com>
2020-07-25 16:17:02 +0300

    toggole hide and show menu lists onclick

---
+++ App.js
@@ -0,0 +1,118 @@
+// breakfast
+// var h = document.getElementsByClassName("breakfast");
+// for(var i=0;i<h.length;i++){
+//     h[i].style.display="none";
+// }
+
+var h = document.querySelector(".breakfast");
+h.style.display="none";
+
+
+document.querySelector(".br").onclick = function(){
+    // var f = document.querySelector(".breakfast");
+    if (h.style.display === "none") {
+              h.style.display = "block";
+            } else {
+              h.style.display = "none";
+            }
+};
+
+// snacks
+var s = document.querySelector(".snacks");
+s.style.display="none";
+
+document.querySelector(".sn").onclick = function(){
+    // var x = document.querySelector(".snacks");
+    if (s.style.display === "none") {
+              s.style.display = "block";
+            } else {
+              s.style.display = "none";
+            }
+};
+
+
+
+// lunch
+var l = document.querySelector(".lunch");
+l.style.display="none";
+
+document.querySelector(".lh").onclick = function(){
+    // var x = document.querySelector(".snacks");
+    if (l.style.display === "none") {
+              l.style.display = "block";
+            } else {
+              l.style.display = "none";
+            }
+};
+
+
+
+// dessert
+var d = document.querySelector(".dessert");
+d.style.display="none";
+
+document.querySelector(".ds").onclick = function(){
+    // var x = document.querySelector(".snacks");
+    if (d.style.display === "none") {
+              d.style.display = "block";
+            } else {
+              d.style.display = "none";
+            }
+};
+
+
+// drinks
+var y = document.querySelector(".drinks");
+y.style.display="none";
+
+document.querySelector(".dr").onclick = function(){
+    // var x = document.querySelector(".snacks");
+    if (y.style.display === "none") {
+              y.style.display = "block";
+            } else {
+              y.style.display = "none";
+            }
+};
+
+
+//dinner
+var p = document.querySelector(".dinner");
+p.style.display="none";
+
+document.querySelector(".din").onclick = function(){
+    // var x = document.querySelector(".snacks");
+    if (p.style.display === "none") {
+              p.style.display = "block";
+            } else {
+              p.style.display = "none";
+            }
+};
+
+
+// soups
+var q = document.querySelector(".soup");
+q.style.display="none";
+
+document.querySelector(".su").onclick = function(){
+    // var x = document.querySelector(".snacks");
+    if (q.style.display === "none") {
+              q.style.display = "block";
+            } else {
+              q.style.display = "none";
+            }
+};
+
+
+
+
+// f.style.display = "none";
+
+// function myFunction() {
+//     var x = document.querySelector(".breakfast");
+//     if (f.style.display === "none") {
+//       f.style.display = "block";
+//     } else {
+//       f.style.display = "none";
+//     }
+//   }
+
