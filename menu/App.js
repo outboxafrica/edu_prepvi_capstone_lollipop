f72e90b04355a78aeed3cd520353cb21e49cdbe9
elywin <lukeeraelywin@gmail.com>
2020-07-27 00:39:55 +0300

    created filter on search bar

--- menu/App.js
+++ menu/App.js
@@ -108,6 +108,21 @@ document.querySelector(".su").onclick = function(){
 
 
 
+// search bar
+function search_animal() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByTagName('tr'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item";                  
+        } 
+    } 
+} 
 
 
 
