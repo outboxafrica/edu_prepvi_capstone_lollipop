6e4a38ac85ca029dc45b0b5f9ee9ad4d56ea39c4
elywin <lukeeraelywin@gmail.com>
2020-07-27 14:04:46 +0300

    organised c code

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 var btn1 = document.querySelector(".loginbtn");
-btn1.onclick = ()=>{
-    location.href="#";
+btn1.onclick = () => {
+    location.href = "#";
 };
 
 var btn2 = document.querySelector(".signupbtn");
-btn2.onclick = ()=>{
-    location.href="#";
+btn2.onclick = () => {
+    location.href = "#";
 };
