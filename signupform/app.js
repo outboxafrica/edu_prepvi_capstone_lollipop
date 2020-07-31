ebf0c5dac0f5886bd173c9d66e7835275adf3311
elywin <lukeeraelywin@gmail.com>
2020-07-31 17:53:13 +0300

    added signup validation

--- signupform/app.js
+++ signupform/app.js
@@ -2,24 +2,31 @@
 var nm = document.getElementById('nm');
 var pw = document.getElementById('pw');
 var pw2 = document.getElementById('pw2');
+var emails=document.getElementById('emails');
 // storing input from register-form
 function store() {
    
     localStorage.setItem('nm', nm.value);
     localStorage.setItem('pw', pw.value);
     localStorage.setItem('pw2', pw2.value);
+    localStorage.setItem('emails', emails.value);
     var storedPw = localStorage.getItem('pw');
     var storedPw2 = localStorage.getItem('pw2');
     
-    if( storedPw.value == storedPw2.value){
+if(nm.value=="" || pw.value==""||pw2.value==""||emails.value=="" ){
+    alert('error please fill in all the fields')
+}else{
+
+    if(storedPw == storedPw2){
         alert("signup successful");
         window.location="../home/home.html";
-      
+        event.preventDefault();
     }else{
         alert(".error");
     }
+}
     
     
-};
+}
 
 
